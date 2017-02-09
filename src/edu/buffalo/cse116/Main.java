fe80e100381ba33a9de955f84f31e27a237b5f2a
rondo <rondo@DESKTOP-5HHUB4K>
2017-02-08 19:18:29 -0500

    Erased print lines

--- src/edu/buffalo/cse116/Main.java
+++ src/edu/buffalo/cse116/Main.java
@@ -4,10 +4,7 @@ public class Main {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-	  System.out.println("Candy Crush is trash.");
-	  System.out.println("Agreed");
-	  System.out.println("Bejeweled is better");
-	  System.out.println("This is kunyu.");
+	  
   }
 
 }
