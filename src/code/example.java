4548a69c0bd3eeb504a4b60d2d65e7c2bc7f3a54
kunyuwu <kunyuwu@buffalo.edu>
2017-02-18 12:32:43 -0500

    Julia set finished
    Hello all,
    I already finished the code of Julia Set. If you check the code, you will find a new .java file. It is the class of doublePoint. I created it since x and y coordinate always appear at the same time, but the existing class Point only can deal with the type of int. You can use this class in your part and in the future for convenience.

--- src/code/example.java
+++ src/code/example.java
@@ -2,4 +2,11 @@ package code;
 
 public class example {
 
+	public doublePoint juliaSet(doublePoint p) {
+		doublePoint juliaPoint = new doublePoint();
+		juliaPoint.x = Math.pow(p.x, 2.0) +  Math.pow(p.y, 2.0) - 0.72689;
+		juliaPoint.y = 2.0 * p.x * p.y + 0.188887;
+		return juliaPoint;
+	}
+	
 }
