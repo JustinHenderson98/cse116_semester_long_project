fcef5cae49398b544c71dea9f82ff70d6f2108f1
ethangag <ethangag@buffalo.edu>
2017-02-24 16:19:08 -0500

    quick fix

--- src/tests/escapetimeTests.java
+++ src/tests/escapetimeTests.java
@@ -16,7 +16,7 @@ public class escapetimeTests {
 		p1.x = 0.3207031250000001;
 		p1.y = -0.07109374999999386;
 		int output = mandlebrotTest.escapeTime(1, p1, 2, 255);
-		assertEquals(255, output, 0.01);
+		assertEquals(255, output);
 	}
 	
 	@Test
@@ -26,7 +26,7 @@ public class escapetimeTests {
 		p2.x = 1.0492187499999897;
 		p2.y = -0.234375;
 		int output = juliaTest.escapeTime(2, p2, 2, 255);
-		assertEquals(255, output, 0.01);
+		assertEquals(255, output);
 	}
 
 	@Test
@@ -36,7 +36,7 @@ public class escapetimeTests {
 		p3.x = -1.7443359374999874;
 		p3.y = -0.017451171875000338;
 		int output = burningshipTest.escapeTime(3, p3, 2, 255);
-		assertEquals(255, output, 0.01);
+		assertEquals(255, output);
 	}
 	
 	@Test
@@ -46,7 +46,7 @@ public class escapetimeTests {
 		p4.x = 0.5859375;
 		p4.y = 0.24375000000000108;
 		int output = multibrotTest.escapeTime(4, p4, 2, 255);
-		assertEquals(255, output, 0.01);
+		assertEquals(255, output);
 	}
 	
 	@Test
@@ -56,7 +56,7 @@ public class escapetimeTests {
 		p6.x = 0.3207031250000001;
 		p6.y = -0.07109374999999386;
 		int output = mandlebrotTest2.escapeTime(1, p6, 2, 0);
-		assertEquals(0, output, 0.01);
+		assertEquals(0, output);
 	}
 	
 	@Test
@@ -66,7 +66,7 @@ public class escapetimeTests {
 		p5.x = 1.0492187499999897;
 		p5.y = -0.234375;
 		int output = juliaTest2.escapeTime(2, p5, 2, 0);
-		assertEquals(0, output, 0.01);
+		assertEquals(0, output);
 	}
 	
 	@Test
@@ -76,7 +76,7 @@ public class escapetimeTests {
 		p7.x = 0.5859375;
 		p7.y = 0.24375000000000108;
 		int output = multibrotTest2.escapeTime(4, p7, 2, 0);
-		assertEquals(0, output, 0.01);
+		assertEquals(0, output);
 	}
 	
 	@Test
