303b13bf2f78e7fac9be9fa24ab7f3cf0efe33c4
rondo <rondo@DESKTOP-5HHUB4K>
2017-03-18 14:48:19 -0400

    updated run.txt and update.txt with proper directions.

--- src/edu/buffalo/cse116/Main.java
+++ src/edu/buffalo/cse116/Main.java
@@ -2,22 +2,10 @@ package edu.buffalo.cse116;
 
 
 import code.*;
+import UI.*;
 public class Main {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-	  generateFractal genFrac = new generateFractal();
-	  
-	  int[][] pot = genFrac.genFractal();
-	  for (int i = 0; i < pot.length; i++) {
-		  System.out.println("");
-		for (int j = 0; j < pot.length; j++) {
-			System.out.print(pot[i][j]);
-			
-			
-		}
-	}
-	  
+	  new userInterface();
   }
-
 }
