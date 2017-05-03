1799c1c470e8aa5a2583c8fd8c436dc3984f2351
Ben Conomos <brconomo@buffalo.edu>
2017-05-03 10:38:36 -0400

    Updated fractalWorker and mainModel

--- src/code/FractalWorker.java
+++ src/code/FractalWorker.java
@@ -8,6 +8,11 @@ public class FractalWorker extends SwingWorker<WorkerResult, Void> {
 	private int _numThreads;
 	private int _id;
 	//gettersetters
+	
+	public FractalWorker(int threads, int id) {
+		_id = id;
+		_numThreads = threads;
+	}
 	@Override
 	protected WorkerResult doInBackground() throws Exception {
 		//generate fractal with genFractal in bounds of thread
--- src/code/mainModel.java
+++ src/code/mainModel.java
@@ -1,6 +1,10 @@
 package code;
 
+import javax.swing.SwingWorker;
+
 import UI.userInterface;
+import edu.buffalo.fractal.ComputePool;
+import edu.buffalo.fractal.WorkerResult;
 
 public class mainModel {
 	private int _numThreads;
@@ -11,14 +15,13 @@ public class mainModel {
 		updateThreads(numThreads);
 	}
 	public void updateThreads(int numThreads) {
-	_numThreads = numThreads;
-	//create SwingWorker array arr with numThreads length
-	//while smaller than _numThreads
-		//generate FractalWorkers
-		//add worker to arr
-		//endwhile
-	//new computepool comppool
-	//comppool.changePanel(_ui.getFractalPanel)
-	//comppool.generateFractal(_rows, arr)
+		_numThreads = numThreads;
+		SwingWorker<WorkerResult, Void> Workers[] = new SwingWorker[_numThreads];//create SwingWorker array arr with numThreads length
+		for (int i = 0; i < _numThreads; i++) {
+			Workers[i] = new FractalWorker(_numThreads, i);
+		}
+		ComputePool comppool = new ComputePool();
+		comppool.changePanel(_ui.getFractalPanel);
+		comppool.generateFractal(_rows, Workers);
 	}
 }
